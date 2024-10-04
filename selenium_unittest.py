1895808d28e40bb8246635179317ef963d1abad9
gregy <mailofgregory@gmail.com>
2024-10-05 01:38:19 +0300

    fix: 2.5 hours

--- selenium_unittest.py
+++ selenium_unittest.py
@@ -6,6 +6,25 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+from datetime import datetime
+
+def fibonacci_of(n):
+    # Validate the value of n
+    if not (isinstance(n, int) and n >= 0):
+        raise ValueError(f'Positive integer number expected, got "{n}"')
+
+
+    # Handle the base cases
+    if n in {0, 1}:
+        return n
+
+
+    previous, fib_number = 0, 1
+    for _ in range(2, n + 1):
+        # Compute the next Fibonacci number, remember the previous one
+        previous, fib_number = fib_number, previous + fib_number
+
+    return previous
 
 service = Service()
 options = webdriver.ChromeOptions()
@@ -24,7 +43,8 @@ try:
 
     WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.CSS_SELECTOR,".btn.btn-default"))).click()
     time.sleep(2)
-
+    
+    # method of pressing button
     buts = driver.find_elements(By.CSS_SELECTOR,".btn.btn-lg.tab")
     for each in buts:
         if "btnClass2" == each.get_dom_attribute("ng-class"):
@@ -36,17 +56,85 @@ try:
     
     input_deposit = driver.find_element(By.XPATH, "//input[@type='number']")
     print(buts) 
-    #input_deposit = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 
-   #     "input.form-control.ng-dirty.ng-valid-number.ng-invalid.ng-invalid-required.ng-touched"))).click()
-
     print(input_deposit)
-#    input_deposit.click()
-    input_deposit.clear()
-    input_deposit.send_keys("1234")
     
+    input_deposit.clear()
+    _today = datetime.today().day + 1
+    calc = fibonacci_of(_today)
+    print(_today, calc)
+    input_deposit.send_keys(str(calc))    
     time.sleep(2)
-    #"form-control.ng-dirty.ng-valid-number.ng-invalid.ng-invalid-required.ng-touched"
+
+    deposit_btn = driver.find_element(By.XPATH, "//button[@type='submit']")
+    print(deposit_btn)
+    
+    deposit_btn.click()
+    time.sleep(2)
+    
+    label = driver.find_element(By.CSS_SELECTOR,"span.error.ng-binding")
+    assert label.text == 'Deposit Successful'
+    time.sleep(2)
+
+    ## test
+    print("Here goes")
+    general_bar = driver.find_elements(By.XPATH, "//div[@class='center']//strong[2]")
+    print(general_bar)
+    print(general_bar[0].text)
+    ## test
+
+
+    # method of pressing button
+    buts = driver.find_elements(By.CSS_SELECTOR,".btn.btn-lg.tab")
+    for each in buts:
+        if "btnClass3" == each.get_dom_attribute("ng-class"):
+            deposit = each 
+        print(each.get_dom_attribute("ng-class"))
+
+    deposit.click()
+    time.sleep(2)
+
+    # repeating code below
+    input_deposit = driver.find_element(By.XPATH, "//input[@type='number']")
+    print(buts) 
+    print(input_deposit)
+    
+    input_deposit.clear()
+    time.sleep(2)
+    input_deposit.send_keys(str(calc))    
+    time.sleep(2)
+
+    deposit_btn = driver.find_element(By.XPATH, "//button[@type='submit']")
+    print(deposit_btn)
+    
+    deposit_btn.click()
+    time.sleep(2)
+
+    balance = driver.find_element(By.XPATH, "//div[@class='center']//strong[2]")
+    assert balance.text == '0'
+    
+    # GO TO TRANSACTIONS PAGE
+
+    # method of pressing button
+    buts = driver.find_elements(By.CSS_SELECTOR,".btn.btn-lg.tab")
+    for each in buts:
+        if "btnClass1" == each.get_dom_attribute("ng-class"):
+            deposit = each 
+        print(each.get_dom_attribute("ng-class"))
+
+    deposit.click()
+    time.sleep(2)
+
+    table = driver.find_element(By.CSS_SELECTOR,".table.table-bordered.table-striped")
+
+    rows = table.find_elements(By.TAG_NAME, "tr")
+    for row in rows:
+        print(row)
+        cells = row.find_elements(By.TAG_NAME, 'td')
+        for td in cells:
+            print(td.text, end=' ')
+        print(end='\n')
+
+    #deposit_btn = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.CSS_SELECTOR,".btn.btn-default"))).click()
 finally:
     driver.quit()
-
 
